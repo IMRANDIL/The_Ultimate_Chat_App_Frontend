87eb4cf07aaa8448834078d8401f139b99ae3cd6
IMRANDIL <aliimranadil2@gmail.com>
2023-05-28 12:10:29 +0530

    some more feature added

--- src/components/ChatComponents/UserListItem.tsx
+++ src/components/ChatComponents/UserListItem.tsx
@@ -1,8 +1,41 @@
+import { Avatar, Box, Text } from "@chakra-ui/react";
 import React from "react";
 
 const UserListItem: React.FC = ({ user, handleChatCreation }) => {
-  console.log(user);
-  return <div>UserListItem</div>;
+  return (
+    <Box
+      onClick={handleChatCreation}
+      cursor={"pointer"}
+      bg={"#E8E8E*"}
+      _hover={{
+        background: "#38B2AC",
+        color: "white",
+      }}
+      width={"100%"}
+      display={"flex"}
+      alignItems={"center"}
+      color="black"
+      px={3}
+      py={2}
+      mb={2}
+      borderRadius={"lg"}
+    >
+      <Avatar
+        mr={2}
+        size={"sm"}
+        cursor={"pointer"}
+        src={user.profilePic}
+        name={user.username}
+      />
+      <Box>
+        <Text>{user.username}</Text>
+        <Text fontSize={"xs"}>
+          <b>Email: </b>
+          {user.email}
+        </Text>
+      </Box>
+    </Box>
+  );
 };
 
 export default UserListItem;
