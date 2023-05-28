aeffede5aa062f606fff0909932af4f19e7ce74f
IMRANDIL <aliimranadil2@gmail.com>
2023-05-28 07:07:10 +0530

    added some more style

--- src/components/ChatComponents/SideDrawer.tsx
+++ src/components/ChatComponents/SideDrawer.tsx
@@ -10,11 +10,21 @@ const SideDrawer: React.FC = () => {
 
   return (
     <>
-      <Box>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        bg="white"
+        w="100%"
+        p="5px 10px 5px 10px"
+        borderWidth="5px"
+      >
         <Tooltip label="Search Users to chat" hasArrow placement="bottom-end">
           <Button variant="ghost">
             <i className="fa fa-search"></i>
-            <Text display={{ base: "none", md: "flex" }}>Search User</Text>
+            <Text display={{ base: "none", md: "flex" }} px="4">
+              Search User
+            </Text>
           </Button>
         </Tooltip>
       </Box>
