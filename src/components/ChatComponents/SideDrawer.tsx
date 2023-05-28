9d671193ba70bc4afb90a4ff37fc5bc647fe6dbe
IMRANDIL <aliimranadil2@gmail.com>
2023-05-28 06:58:42 +0530

    some more addition done

--- src/components/ChatComponents/SideDrawer.tsx
+++ src/components/ChatComponents/SideDrawer.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Box } from "@chakra-ui/layout";
-import { Button, Tooltip } from "@chakra-ui/react";
+import { Button, Tooltip, Text } from "@chakra-ui/react";
 
 const SideDrawer: React.FC = () => {
   const [search, setSearch] = useState("");
@@ -14,6 +14,7 @@ const SideDrawer: React.FC = () => {
         <Tooltip label="Search Users to chat" hasArrow placement="bottom-end">
           <Button variant="ghost">
             <i className="fa fa-search"></i>
+            <Text display={{ base: "none", md: "flex" }}>Search User</Text>
           </Button>
         </Tooltip>
       </Box>
--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,21 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ToastContainer } from "react-toastify";
 import { Provider } from "react-redux";
+import { ChakraProvider } from "@chakra-ui/react";
 import store from "./redux/store.ts";
 import "react-toastify/dist/ReactToastify.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "@chakra-ui/css-reset";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <>
     <Provider store={store}>
-      <App />
-      <ToastContainer />
+      <ChakraProvider>
+        <App />
+        <ToastContainer />
+      </ChakraProvider>
     </Provider>
   </>
 );
