de90b9393da040866ac2ed938a60d7d395bce810
IMRANDIL <aliimranadil2@gmail.com>
2023-06-18 08:12:24 +0530

    some more addition to ui logic

--- src/components/ChatComponents/GroupChatModal.tsx
+++ src/components/ChatComponents/GroupChatModal.tsx
@@ -1,7 +1,31 @@
+import {
+  Button,
+  Modal,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 import React from "react";
+import { ModalBody } from "react-bootstrap";
 
 const GroupChatModal: React.FC = ({ children }) => {
-  return <>{children}</>;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Button onClick={onOpen}>{children}</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>eeeeee</ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 };
 
 export default GroupChatModal;
