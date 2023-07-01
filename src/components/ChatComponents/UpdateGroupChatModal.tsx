eae2fd1f1844a6e0a0e6e3c6f68798b58847d0d5
IMRANDIL <aliimranadil2@gmail.com>
2023-07-01 06:31:04 +0530

    added iconButton

--- src/components/ChatComponents/UpdateGroupChatModal.tsx
+++ src/components/ChatComponents/UpdateGroupChatModal.tsx
@@ -1,7 +1,45 @@
+import { ViewIcon } from "@chakra-ui/icons";
+import {
+  Button,
+  IconButton,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 import React from "react";
 
 const UpdateGroupChatModal: React.FC = () => {
-  return <div>UpdateGroupChatModal</div>;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <IconButton
+        onClick={onOpen}
+        icon={<ViewIcon />}
+        display={{ base: "flex" }}
+      />
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>eeeeeee</ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 };
 
 export default UpdateGroupChatModal;
